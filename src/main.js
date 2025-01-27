8a6b2f669ed4d3274654535070f6c474b751922f
Francklin <francklinoklou@gmail.com>
2025-01-27 00:09:59 +0000

    updated features

--- src/main.js
+++ src/main.js
@@ -493,13 +493,13 @@ function check() {
   const inputText = document.querySelector(".text-input").value; // Texte saisi par l'utilisateur
   const textElement = document.querySelector(".full");
   const spans = textElement.getElementsByTagName("span"); // Les <span> du texte cible
-
+  const textContainer = gameManager()
   // Parcourt le texte cible caractère par caractère
-  for (let i = 0; i < textContainer[0].length; i++) {
+  for (let i = 0; i < textContainer.length; i++) {
     if (spans[i]) {
       if (i < inputText.length) {
         // Vérifie si le caractère saisi correspond au caractère cible
-        if (inputText[i] === textContainer[0][i]) {
+        if (inputText[i] === textContainer[i]) {
           spans[i].className = "correct"; // Correct
         } else {
           spans[i].className = "incorrect"; // Incorrect
